0fd8793683bc5869220ad8b43876c60022509621
mi-hye <satge13@gmail.com>
2024-06-21 14:23:42 +0900

    bug: store의 값을 변경할때만 프록시 객체를 이용해서 render작업

--- src/logic/hooks.js
+++ src/logic/hooks.js
@@ -1,21 +1,21 @@
-import { store } from "./Store.js";
+import { store, storeProxy } from "./Store.js";
 
-// let componentIdCounter = 0; // 컴포넌트 ID 생성기
 function useState(initialValue) {
-	// const componentId = componentIdCounter++;
-	// if (!(componentId in globalState)) {
-	// globalState[componentId] = initialValue;
-	// }
-	const key = JSON.stringify(initialValue);
+	const error = new Error();
+	const [, , atCaller] = error.stack.split("\n");
+	const [, caller] = atCaller.trim().split(" "); //호출한 컴포넌트 이름
+
+	const key = caller; //JSON.stringify(initialValue);
 	if (!store[key]) store[key] = initialValue;
 	//state가 없는경우 초기 state 등록
 
 	const setState = (value) => {
 		if (typeof value === "function")
-			store[key] = value(store[key]);
-		else store[key] = value;
+			storeProxy[key] = value(store[key]);
+		else storeProxy[key] = value;
 	};
 
-	return [store[key], setState];
+	return [store[key], setState];	
 }
+
 export { useState };
